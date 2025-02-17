293890091d0114215b4747cd4184318173b793d1
Kaushik Kulkarni <kaushikcfd@gmail.com>
2025-02-16 22:42:57 -0600

    Wrap BigNum class, Graph::canonical_form, DIMACS support.

--- src/pybliss_ext.cpp
+++ src/pybliss_ext.cpp
@@ -1,3 +1,4 @@
+#include <bliss/bignum.hh>
 #include <bliss/graph.hh>
 #include <bliss/stats.hh>
 #include <memory>
@@ -44,6 +45,26 @@ FILE *get_fp_from_writeable_pyobj(nb::object file_obj) {
   return nb::cast<FILE *>(file_obj.attr("fileno")());
 }
 
+FILE *get_fp_from_readable_pyobj(nb::object file_obj) {
+  // Check if the object is None (indicating no file)
+  if (file_obj.is_none()) {
+    throw std::runtime_error("File object is None");
+  }
+
+  // Check if the object has a 'fileno' method
+  if (!nb::hasattr(file_obj, "fileno")) {
+    throw std::runtime_error(
+        "Expected a file-like object with a 'fileno' method");
+  }
+
+  // Check if the object is writable
+  if (!nb::hasattr(file_obj, "read")) {
+    throw std::runtime_error(
+        "File object is not readable (missing 'read' method)");
+  }
+  return nb::cast<FILE *>(file_obj.attr("fileno")());
+}
+
 std::string
 capture_string_written_to_file(std::function<void(FILE *)> file_writer) {
   // unique_ptr to ensure FILE* is properly closed
@@ -79,7 +100,32 @@ NB_MODULE(pybliss_ext, m) {
   m.def(
       "add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
 
-  nb::class_<Stats>(m, "Stats")
+  // {{{ BigNum
+
+  nb::class_<BigNum>(m, "BigNum",
+                     "A wrapper class for non-negative big integers.")
+      .def(nb::init<>())
+      .def("assign", &BigNum::assign)
+      .def("multiply", &BigNum::multiply)
+      .def("print_to_file",
+           [](Stats &self, nb::object fp_obj) {
+             FILE *fp = get_fp_from_writeable_pyobj(fp_obj);
+             self.print(fp);
+           })
+      .def("__str__",
+           [](Stats &self) {
+             return capture_string_written_to_file(
+                 [&](FILE *fp) { self.print(fp); });
+           }
+           // FIXME: Add a to_python method.
+      );
+
+  // }}}
+
+  // {{{ Stats
+
+  nb::class_<Stats>(m, "Stats",
+                    "Records statistics returned by the search algorithms.")
       .def(nb::init<>())
       .def("print_to_file",
            [](Stats &self, nb::object fp_obj) {
@@ -117,7 +163,13 @@ NB_MODULE(pybliss_ext, m) {
             [&](FILE *fp) { self.print(fp); });
       });
 
-  nb::class_<Graph>(m, "Graph")
+  // }}}
+
+  // {{{ Graph
+
+  nb::class_<Graph>(m, "Graph",
+                    "Undirected vertex-colored graph.\n\n"
+                    "See :class:`DiGraph` for a directed variant.")
       .def(nb::init<>())
       .def(
           "set_verbose_level", &Graph::set_verbose_level, "level"_a,
@@ -140,13 +192,12 @@ NB_MODULE(pybliss_ext, m) {
           "writing to the file is disabled.")
       .def("add_vertex", &Graph::add_vertex, "color"_a = 0,
            "Add a new vertex with color *color* and return its new index.")
-      .def("add_edge", &Graph::add_edge, "source"_a, "target"_a,
-           "Add an edge from *source* to *target*.")
+      .def("add_edge", &Graph::add_edge, "v1"_a, "v1"_a,
+           "Add an edge between *v1* and *v2*.")
       .def("get_color", &Graph::get_color, "v"_a,
            "Returns the color of the vertex *v*")
       .def("change_color", &Graph::change_color, "v"_a, "c"_a,
            "Change the color of vertex *v* to *c*.")
-      // TODO: Think if this should not be exposed.
       .def("set_failure_recording", &Graph::set_failure_recording, "active"_a,
            "Activate / deactivate failure recording\n\n"
            ":arg active:If true, activate failure recording, deactivate "
@@ -217,11 +268,9 @@ NB_MODULE(pybliss_ext, m) {
           "generator for the automorphism group is found. The first argument "
           "*n* for the function is the length of the automorphism (equal to "
           "get_nof_vertices()), and the second argument *aut* is the "
-          "automorphism (a bijection on {0,...,nvertices-1}). The memory for "
-          "the automorphism *aut* will be invalidated immediately after the "
-          "return from the *report* function; if you want to use the "
-          "automorphism later, you have to take a copy of it. Do not call any "
-          "member functions from the *report* function.\n\n"
+          "automorphism (a bijection on {0,...,nvertices-1}). *aut* is a "
+          "read-only :class:`numpy.ndarray`. Do not call any member functions "
+          "from the *report* function.\n\n"
 
           "The search statistics are copied in *stats*.\n\n"
 
@@ -231,6 +280,106 @@ NB_MODULE(pybliss_ext, m) {
           "generated. The *terminate* function may be used to limit the time "
           "spent in bliss in case the graph is too difficult under the "
           "available time constraints. If used, keep the function simple to "
-          "evaluate so that it does not consume too much time. "
-      );
+          "evaluate so that it does not consume too much time. ")
+      .def(
+          "get_permutation_to_canonical_form",
+          [](Graph &self, Stats &stats,
+             const std::function<void(
+                 int, nb::ndarray<nb::ro, uint32_t, nb::ndim<1>, nb::numpy,
+                                  nb::c_contig>)> &py_report = nullptr,
+             const std::function<bool()> &py_terminate = nullptr) {
+            std::function<void(unsigned int, const unsigned int *)> cpp_report =
+                nullptr;
+            std::function<bool()> cpp_terminate = nullptr;
+
+            if (py_report) {
+              cpp_report = [&](unsigned int n, const unsigned int *aut) {
+                auto np_aut =
+                    nb::ndarray<nb::ro, uint32_t, nb::ndim<1>, nb::numpy,
+                                nb::c_contig>(aut, {self.get_nof_vertices()});
+                py_report(n, np_aut);
+              };
+            }
+
+            if (py_terminate) {
+              cpp_terminate = [&]() { return py_terminate(); };
+            }
+
+            auto perm = self.canonical_form(stats, cpp_report, cpp_terminate);
+            // FIXME: Wrap perm in a Numpy class.
+          },
+          "stats"_a, "report"_a = nb::none(), "terminate"_a = nb::none(),
+          "Returns `P`, a :class:`numpy.ndarray` on {0, ..., nvertices-1}. "
+          "Applying the 'permutation `P` to this graph results in this graph's "
+          "canonical graph. The function *report* (if not None) is called each "
+          "time a new  generator for the automorphism group is found. The "
+          "first argument  *n* for the function is the length of the "
+          "automorphism (equal to  get_nof_vertices()), and the second "
+          "argument *aut* is the  automorphism (a bijection on "
+          "{0,...,nvertices-1}). *aut* is a read-only :class:`numpy.ndarray`."
+          " Do not call any  member functions from the *report* function.\n\n"
+
+          "The search statistics are copied in *stats*.\n\n"
+
+          "If the *terminate* function argument is given, it is called in each "
+          "search tree node: if the function returns true, then the search is "
+          "terminated and thus not all the automorphisms may have been "
+          "generated. The *terminate* function may be used to limit the time "
+          "spent in bliss in case the graph is too difficult under the "
+          "available time constraints. If used, keep the function simple to "
+          "evaluate so that it does not consume too much time.\n\n"
+
+          "This wraps Graph::canonical_form from the C++-API.")
+      // FIXME: Make from_dimacs compile-able.
+      // .def_static(
+      //     "from_dimacs",
+      //     [](nb::object file_obj) {
+      //       FILE *fp = get_fp_from_writeable_pyobj(file_obj);
+      //       Graph *ptr = nullptr;
+      //       auto err_str = capture_string_written_to_file(
+      //           [&](FILE *err_stream) { ptr = Graph::read_dimacs(fp,
+      //           err_stream); });
+      //       if (!ptr) {
+      //         throw std::runtime_error(
+      //             "Error during reading Graph from DIMACS.\n" + err_str);
+      //       }
+      //       return std::unique_ptr<Graph *>(ptr);
+      //     },
+      //     "fp"_a,
+      //     "Return a graph corresponding to DIMACS-formatted graph present in
+      //     "
+      //     "*fp*. See the `bliss website "
+      //     "<https://users.aalto.fi/tjunttil/bliss>` for the definition of the
+      //     " "file format. Note that in the DIMACS file the vertices are
+      //     numbered " "from 1 to N while in this API they are from 0 to N-1.
+      //     Thus the " "vertex n in the file corresponds to the vertex n-1 in
+      //     the API.\n\n"
+      //     ":arg fp: The file stream from where the graph is to be read.")
+      .def(
+          "write_dimacs",
+          [](Graph &self, nb::object file_obj) {
+            FILE *fp = get_fp_from_writeable_pyobj(file_obj);
+            self.write_dimacs(fp);
+          },
+          "fp"_a,
+          "Write the graph to *fp* in a variant of the DIMACS format. See "
+          "the `bliss website <https://users.aalto.fi/tjunttil/bliss>` for the "
+          "definition of the file format. Note that in the DIMACS file the "
+          "vertices are numbered from 1 to N while in this API they are from 0 "
+          "to N-1. Thus the vertex n in the file corresponds to the vertex n-1 "
+          "in the API.\n\n"
+          ":arg fp: The file stream where the graph is to be written.")
+
+      .def(
+          "write_dot",
+          [](Graph &self, nb::object file_obj) {
+            FILE *fp = get_fp_from_writeable_pyobj(file_obj);
+            self.write_dot(fp);
+          },
+          "fp"_a,
+          "Write the graph to *fp* in the graphviz format.\n\n"
+          ":arg fp: The file stream where the graph is to be written.")
+      .def("__hash__", &Graph::get_hash);
+
+  // }}}
 }
